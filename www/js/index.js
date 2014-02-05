83f5f2900cf688536d9c9db458b80459e073097f
Alexander Perez <alexanderlperez@gmail.com>
2014-02-05 01:59:18 -0800

    Needs further testing

--- www/js/index.js
+++ www/js/index.js
@@ -17,10 +17,33 @@
  * under the License.
  */
 var app = (function () {
-  function _initialize(mapID, startingLoc) {
+  function _initialize(mapID) {
+    var startingLoc = undefined;
+
+    //getCurrentPosition callbacks
+    var onSuccess = function (position) { 
+      startingLoc = {};
+      startingLoc.lat = position.coords.latitude;
+      startingLoc.lng = position.coords.longitude;
+    };
+
+    var onError = function (error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n' +
+            'Falling back to default location');
+    };
+
+    //try to get the initial position of the user
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, 
+      {
+        maximumAge: 3000,
+        timeout: 5000,
+        enableHighAccuracy: true
+      }
+    );
+
     _createMap(mapID, startingLoc);
   }
-
 
   function _createMap(_mapID, _startingLoc) {
     var home;
